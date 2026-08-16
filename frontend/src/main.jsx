2ae94d1c8bdcb6695e08192b8cb8653c255fd05d
Your Name <sohom5102@gmail.com>
2026-08-17 00:06:04 +0600

    fix: switch to HashRouter so page refresh and direct link navigation never fail with 404 Not Found on static hosting

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
 import { AuthProvider } from './context/AuthContext'
 import App from './App.jsx'
@@ -8,7 +8,7 @@ import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AuthProvider>
         <App />
         <Toaster
@@ -26,6 +26,6 @@ createRoot(document.getElementById('root')).render(
           }}
         />
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 )
